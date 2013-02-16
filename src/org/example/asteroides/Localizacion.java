7508df7b7500b33a06a834f5017611ba453f12f9
Aitor Mendaza <aitor.mendaza@altran.es>
2013-02-16 18:51:13 +0100

    Varias cosas, preferencias, etc

--- src/org/example/asteroides/Localizacion.java
+++ src/org/example/asteroides/Localizacion.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,7 +15,7 @@ public class Localizacion extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		Button bSalir =(Button) findViewById(R.id.Button04);
 		bSalir.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
@@ -30,8 +31,25 @@ public class Localizacion extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		 case R.id.config:
+             lanzarPreferencias(null);
+             break;
+		}
+		return true; /** true -> consumimos el item, no se propaga*/
+	}
+
 	public void lanzarAcercaDe(View view){
 		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+	public void lanzarPreferencias(View view){
+		Intent i = new Intent(this, Preferencias.class);
 		startActivity(i);
 	}
 }
